fd0ea6de0b3db4c255f48e16b31e5cb12eb43915
wangxs2 <15037589151@163.com>
2020-03-13 18:04:45 +0800

    登录两小时未操作自动退出登录

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" @mouseover="OperatingWebsite()">
     <router-view></router-view>
   </div>
 </template>
@@ -7,7 +7,31 @@
 <script>
 
 export default {
-  name: 'app',
+  name: 'app',data() {
+    return {
+      currentTime: new Date().getTime() 
+    };
+  },
+  methods: {
+    OperatingWebsite() {
+      let currentTime = this.currentTime;
+      // console.log(currentTime, "currentTime");
+      let lastTime = new Date().getTime();
+      let timeOut = 120 * 60 * 1000; //设置时间 2小时
+      // console.log(lastTime - currentTime, "lastTime",timeOut );
+      if (lastTime - currentTime > timeOut) {
+        // 未操作页面，跳转登录页面
+        this.currentTime = new Date().getTime(); 
+        this.$router.push({
+          path: "/login",
+        });
+        this.$message.error("登录过期，请重新登录")
+      } else {
+        this.currentTime = new Date().getTime(); 
+      }
+ 
+    }
+  }
 }
 </script>
 
